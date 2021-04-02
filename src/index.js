61240ad28287bb6510933c11fd26ba5d5263944f
swayambhu <swayambhu43@gmail.com>
2021-04-02 19:49:10 +0530

    Added class based components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            lat : null,
+            errorMessage : '',
+        };
+        
+        window.navigator.geolocation.getCurrentPosition(
+            (position) =>{
+                //setState is used to update the state
+                this.setState({lat: position.coords.latitude});
+            },
+            
+            (err) => {
+                this.setState({errorMessage : err.message});
+            }
+        )
+    }
+    render(){    
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error : {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <div>Latitude : {this.state.lat}</div>
+        }
+        if(!this.state.errorMessage && !this.state.lat){
+            return <div>Loading!</div>
+        }
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
